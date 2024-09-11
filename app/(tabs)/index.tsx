31486d83b67b62cb5d655ec084e81ca63af07854
Saul Lee <solleedata@gmail.com>
2024-09-12 01:42:55 +0900

    feat: Get real data from supabase for the community page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import {
-  Dimensions,
   ScrollView,
   StyleSheet,
   Text,
@@ -8,98 +7,107 @@ import {
   View,
 } from 'react-native';
 import { useRouter } from 'expo-router';
-import { ChatText, Heart, Plus } from 'phosphor-react-native';
+import { useFocusEffect } from '@react-navigation/native';
+import { supabase } from '@/supabaseClient';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import AddButton from '@/components/buttons/Add';
+import { ChatText, Heart, Plus } from 'phosphor-react-native';
 
-export default function Feed(props) {
-  const [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: 'First Post',
-      createdAt: '2024-09-01',
-      likes: 12,
-      comments: 4,
-    },
-    {
-      id: 2,
-      title: 'Second Post',
-      createdAt: '2024-09-05',
-      likes: 25,
-      comments: 10,
-    },
-    {
-      id: 3,
-      title: 'Third Post',
-      createdAt: '2024-09-07',
-      likes: 8,
-      comments: 2,
-    },
-    {
-      id: 4,
-      title:
-        'Fourth Post - This post has a longer title for testing purposes to see how it looks when it exceeds 80 characters.',
-      createdAt: '2024-09-10',
-      likes: 45,
-      comments: 15,
-    },
-    {
-      id: 5,
-      title:
-        'Fourth Post - This post has a longer title for testing purposes to see how it looks when it exceeds 80 characters.',
-      createdAt: '2024-09-10',
-      likes: 45,
-      comments: 15,
-    },
-  ]);
+interface Post {
+  id: string;
+  title: string;
+  content: string;
+  created_at: string;
+  likes: number;
+  comments: number;
+  author_id: string;
+}
 
+export default function Feed() {
+  const [posts, setPosts] = useState<Post[]>([]);
   const router = useRouter();
+
+  async function fetchPosts() {
+    try {
+      const userId = await AsyncStorage.getItem('@dailykpop-user');
+      if (!userId) {
+        console.log('No user ID found');
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from('posts')
+        .select('*')
+        // .eq('author_id', userId) // Match with userId
+        .order('created_at', { ascending: false });
+
+      if (error) throw error;
+      setPosts(data);
+    } catch (e) {
+      console.log('Error fetching posts:', e.message);
+    }
+  }
+
+  // Use useFocusEffect to fetch posts whenever the screen comes into focus
+  useFocusEffect(
+    useCallback(() => {
+      fetchPosts(); // Fetch posts on screen focus
+    }, [])
+  );
 
   return (
     <SafeAreaView style={styles.safeareaview}>
       <ScrollView>
         <View style={styles.container}>
-          {posts
-            .sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt))
-            .map((post, index) => (
-              <View key={index} style={styles.post}>
-                <TouchableOpacity
-                  onPress={() => router.push('/detailedFeed', { param: post })}
-                >
-                  <View style={styles.post}>
-                    <View style={styles.content}>
-                      <Text style={styles.text}>
-                        {post.title.length > 80
-                          ? post.title.substring(0, 80) + '...'
-                          : post.title}
-                      </Text>
-                    </View>
-                    <View style={styles.postFooter}>
-                      <Text style={styles.createdAt}>{post.createdAt}</Text>
-                      <View style={styles.stat}>
-                        <View style={styles.statDetails}>
-                          <Heart size={18} color='black' />
-                          <Text> {post.likes} </Text>
-                        </View>
-                        <View style={styles.statDetails}>
-                          <ChatText size={18} color='black' />
-                          <Text> {post.comments} </Text>
-                        </View>
+          {posts.map((post) => (
+            <View key={post.id} style={styles.post}>
+              <TouchableOpacity
+                onPress={() =>
+                  router.push({
+                    pathname: '/detailedFeed',
+                    params: { param: post },
+                  })
+                }
+              >
+                <View style={styles.post}>
+                  <View style={styles.content}>
+                    <Text style={styles.title}>
+                      {post.title.length > 80
+                        ? post.title.substring(0, 80) + '...'
+                        : post.title}
+                    </Text>
+                    <Text style={styles.text}>
+                      {post.content.length > 120
+                        ? post.content.substring(0, 120) + '...'
+                        : post.content}
+                    </Text>
+                  </View>
+                  <View style={styles.postFooter}>
+                    <Text style={styles.createdAt}>
+                      {post.created_at.toString().split('T')[0]}
+                    </Text>
+                    <View style={styles.stat}>
+                      <View style={styles.statDetails}>
+                        <Heart size={18} color='black' />
+                        <Text> {post.likes} </Text>
+                      </View>
+                      <View style={styles.statDetails}>
+                        <ChatText size={18} color='black' />
+                        <Text> {post.comments} </Text>
                       </View>
                     </View>
                   </View>
-                </TouchableOpacity>
-              </View>
-            ))}
+                </View>
+              </TouchableOpacity>
+            </View>
+          ))}
         </View>
       </ScrollView>
-
-      <AddButton routeName='/addPost' />
+      <AddButton routeName='/addFeed' />
     </SafeAreaView>
   );
 }
-
-const WIDTH = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   safeareaview: {
@@ -107,40 +115,33 @@ const styles = StyleSheet.create({
   },
   container: {
     backgroundColor: '#eee',
+    marginBottom: 80,
   },
   post: {
     marginTop: 10,
     backgroundColor: '#fff',
-    padding: 15, // Added padding for post content
-    borderRadius: 10, // Added border-radius for better UI
-    shadowColor: 'gray', // Added shadow for better UI
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 3,
-    elevation: 2,
-  },
-  createdAt: {
-    marginLeft: 12,
-    color: 'gray',
-    fontSize: 12,
   },
   content: {
     marginHorizontal: 20,
-    marginVertical: 10,
-    color: 'gray',
+    marginVertical: 20,
   },
-  text: {
+  title: {
     fontSize: 16,
     color: '#02007F',
+  },
+  text: {
+    fontSize: 14,
+  },
+  createdAt: {
+    fontSize: 12,
   },
   postFooter: {
     marginTop: 10,
     flexDirection: 'row',
+    padding: 20,
   },
   stat: {
     flexDirection: 'row',
-    marginLeft: 5,
-    marginRight: 5,
     flex: 1,
     justifyContent: 'flex-end',
   },
@@ -149,8 +150,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#eee',
     borderRadius: 13,
     marginHorizontal: 5,
-    fontSize: 12,
-    fontWeight: '600',
+    fontSize: 10,
     flexDirection: 'row',
     alignItems: 'center', // Ensure icons and text are centered
   },
