19e397220caa41252f74ce5d3656a3f69e2e5aba
Saul Lee <solleedata@gmail.com>
2024-09-14 21:04:12 +0900

    fix: placeholder text color visible

--- components/buttons/Add.tsx
+++ components/buttons/Add.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   floatingBtnContainer: {
     justifyContent: 'center',
     paddingHorizontal: 20,
-    top: Platform.OS === 'ios' ? 20 : 0,
+    top: Platform.OS === 'ios' ? 10 : 0,
   },
   floatingBtn: {
     borderWidth: 1,
